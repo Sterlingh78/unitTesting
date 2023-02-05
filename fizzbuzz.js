1c5f5ea166f7cb4a2d52b27df774762718b28080
sterlingh78 <sterlingh78@yahoo.com>
2023-02-05 16:51:34 -0700

    fizzbuzz commit-changed method to strict

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,25 +3,22 @@ const fizzbuzz = (n) => {
 
     for (let i = 0; i <= n; i++) {
         if (i !== 0) {
-            if (i / 3 === 0) {
-                arr.splice(i, 0, 'fizz')
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    arr.splice(i, 1, 'fizzbuzz')
+                    continue
+                } else {
+                    arr.splice(i, 0, 'fizz')
+                }
             }
-            if (i / 5 === 0) {
+            if (i % 5 == 0) {
                 arr.splice(i, 1, 'buzz')
             }
-            if (i / 3 === 0 && i / 5 === 0) {
-                arr.splice(i, 1, 'fizzbuzz')
-            }
-            if (i / 3 !== 0 && i / 5 !== 0) {
+            if (i % 3 !== 0 && i % 5 !== 0) {
                 arr.splice(i, 0, i)
             }
-        } else {
-            arr.push(i)
         }
     }
     return arr
 }
-
-console.log(fizzbuzz(15))
-
 module.exports = fizzbuzz;
--- functions.test.js
+++ functions.test.js
@@ -9,5 +9,5 @@ test('adds 1 + 2 to equal 3', () => {
 
 //Fizzbuzz
 test('3 = fizz; 5 = buzz; 3 or 5 = fizzbuzz' , () => {
-    expect(fizzbuzz(15)).toBe([1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
+    expect(fizzbuzz(15)).toStrictEqual([1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
 })

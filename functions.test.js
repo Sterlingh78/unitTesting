eebd22f64cc1c9cb5ee5c71bad435c7cc68be121
sterlingh78 <sterlingh78@yahoo.com>
2023-02-05 15:42:23 -0700

    fizzbuzz test commit

--- functions.test.js
+++ functions.test.js
@@ -6,3 +6,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+//Fizzbuzz
+test('3 = fizz; 5 = buzz; 3 or 5 = fizzbuzz' , () => {
+    expect(fizzbuzz(15)).toBe([1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
+})

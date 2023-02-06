51cfb83cb8bb3d0314d3bcd0af7872bcb61441c7
sterlingh78 <sterlingh78@yahoo.com>
2023-02-05 17:01:42 -0700

    addition commit

--- functions.js
+++
@@ -1,5 +0,0 @@
-//Sample function
-function sum(a, b) {
-    return a + b;
-}
-module.exports = sum;
--- functions.test.js
+++ functions.test.js
@@ -1,13 +1,14 @@
-const sum = require('./functions');
 const fizzbuzz = require('./fizzbuzz')
+const addition = require('./addition')
 
-
-// Example function
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-//Fizzbuzz
+// *Fizzbuzz*
 test('3 = fizz; 5 = buzz; 3 or 5 = fizzbuzz' , () => {
     expect(fizzbuzz(15)).toStrictEqual([1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
-})
+});
+
+// *Calculator*
+
+// Addition
+test('adds 3 + 5 to equal 8', () => {
+    expect(addition(3, 5)).toBe(8);
+});

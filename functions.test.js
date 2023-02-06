3a7255c17486e8ff2f097d719a852d4aecfc4790
sterlingh78 <sterlingh78@yahoo.com>
2023-02-05 18:22:13 -0700

    type checks commit

--- functions.test.js
+++ functions.test.js
@@ -10,6 +10,11 @@ test('3 = fizz; 5 = buzz; 3 or 5 = fizzbuzz' , () => {
     expect(fizzbuzz(15)).toStrictEqual([1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
 });
 
+//Check for array
+test('test for array returned', () => {
+    expect(typeof fizzbuzz(15)).toBe("object")
+});
+
 // *Calculator*
 
 // Addition
@@ -17,9 +22,19 @@ test('adds 3 + 5 to equal 8', () => {
     expect(calculator(3, 5, '+')).toBe(8);
 });
 
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(3, 5, '+')).toBe("number")
+});
+
 // Subtraction
 test('subtracts 3 from 5 to equal 2', () => {
     expect(calculator(3, 5, '-')).toBe(2);
+});
+
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(3, 5, '-')).toBe("number")
 });
 
 // Multiplication
@@ -27,9 +42,19 @@ test('multiply 3 with 5 to equal 15', () => {
     expect(calculator(3, 5, '*')).toBe(15);
 });
 
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(3, 5, '*')).toBe("number")
+});
+
 // Division
 test('divide 15 by 3 to equal 5', () => {
     expect(calculator(15, 3, '/')).toBe(5);
+});
+
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(15, 3, '/')).toBe("number")
 });
 
 // Square
@@ -37,9 +62,20 @@ test('square 5 to get 25', () => {
     expect(calculator(5, '^')).toBe(25);
 });
 
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(5, '^')).toBe("number")
+});
+
+
 // Square root
 test('return square root of 25 which is 5', () => {
     expect(calculator(25, '√')).toBe(5);
+});
+
+//Check for number
+test('test for number returned', () => {
+    expect(typeof calculator(25, '√')).toBe("number")
 });
 
 // *Strings*
@@ -49,14 +85,29 @@ test('reverse input string', () => {
     expect(strings('string', 'reverse')).toBe('gnirts');
 });
 
+//Check for string
+test('test for string returned', () => {
+    expect(typeof strings('string', 'reverse')).toBe("string")
+});
+
 // Uppercase string
 test('uppercase string', () => {
     expect(strings('string', 'upper')).toBe('STRING');
 });
 
+//Check for string
+test('test for string returned', () => {
+    expect(typeof strings('string', 'upper')).toBe("string")
+});
+
 // Remove vowels from string
 test('remove vowels from string', () => {
     expect(strings('string', 'remove')).toBe('strng');
+});
+
+//Check for string
+test('test for string returned', () => {
+    expect(typeof strings('string', 'remove')).toBe("string")
 });
 
 // *Object validator*
@@ -68,8 +119,20 @@ test('check if key is in object', () => {
     })).toBe(true);
 });
 
+// Check for boolean
+test('test for boolean', () => {
+    expect(typeof validateObj('name', {
+        person: "sterling",
+        name: "holt"
+    })).toBe("boolean")
+});
+
 // *Sum numbers*
 
 test('sum numbers in given array', () => {
     expect(sumNums([1,2,3,4,5])).toBe(15);
+});
+
+test('test for number returned', () => {
+    expect(typeof sumNums([1,2,3,4])).toBe("number")
 });
